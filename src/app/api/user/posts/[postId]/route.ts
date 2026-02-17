c9bdf02bf181acd8a884fd8d5344250da6bfdb83
Aman-jha12 <ajha82703@gmail.com>
2026-02-18 04:32:04 +0530

    Refactor GET route for post retrieval with session validation and error handling

--- src/app/api/user/posts/[postId]/route.ts
+++ src/app/api/user/posts/[postId]/route.ts
@@ -1,24 +1,39 @@
 import { getServerSession } from "next-auth"
 import { NextResponse } from "next/server";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+import { prisma } from "@/lib/prisma";
 
 
 
-
-
-
-
-
-
-export async function POST(req:Request, {params}:{params:{postId:string}}){
-    try{
+export async function GET(
+  req: Request,
+  { params }: { params: { postId: string } }
+) {    try{
      const session=await getServerSession(authOptions);
      if(!session){
         return NextResponse.json({message:"Unauthorized"},{status:401})
      }
 
 
-     
+
+
+      const post=await prisma.post.findFirst({
+        where:{
+            id:params.postId,
+            authorId:session.user.id,
+            isDeleted:false
+         }
+        })
+       
+        
+        if(params.postId!==post?.id){
+            return NextResponse.json({message:"Forbidden"},{status:403})
+        }
+
+        if(!post){
+            return NextResponse.json({message:"Post not found"},{status:404})
+        }
+        return NextResponse.json(post,{status:200})
 
 
 
